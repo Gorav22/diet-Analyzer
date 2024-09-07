bfc567036b7c60863c87eac22384e71ae695b42c
Gorav <145363298+Gorav22@users.noreply.github.com>
2024-09-07 23:03:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ if uploaded_file is not None:
 submit=st.button("Tell me about the total calories")
 
 input_prompt="""
-You are an expert in nutritionist I have given you a image just see the image and tell the calories present in that food in the following format
+You are an expert in nutritionist I have given you a image just tell the calories present in that food in the following format
                1. Item 1 - no of calories
                2. Item 2 - no of calories
                ----
