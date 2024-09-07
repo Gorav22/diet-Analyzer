6627d8b9eb23deb6e7e189dccc91246f732a8d14
Gorav <145363298+Gorav22@users.noreply.github.com>
2024-09-07 17:15:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 
 def get_gemini_response(input_prompt,image,prompt):
-    model=genai.GenerativeModel('gemini-pro-vision')
+    model=genai.GenerativeModel('gemini-1.5-pro')
     response=model.generate_content([input_prompt,image[0],prompt])
     return response.text
 
@@ -31,7 +31,6 @@ def input_image_setup(uploaded_file):
 st.set_page_config(page_title="GLOPO- Calory Advisor APP")
 
 st.header("AI Nutritionist App")
-input=st.text_input("Input Prompt: ",key="input")
 uploaded_file=st.file_uploader("Choose an image", type=["jpg","jpeg","png"])
 image=""
 if uploaded_file is not None:
@@ -41,12 +40,12 @@ if uploaded_file is not None:
 submit=st.button("Tell me about the total calories")
 
 input_prompt="""
-You are an expert in nutritionist where you need to see the food items from the image
-               and calculate the total calories, also provide the details of every food items with calories intake
+You are an expert in nutritionist where you need to see the food items
+               and calculate the total calories and proteins, also provide the details of every food items with calories intake
                is below format
 
-               1. Item 1 - no of calories
-               2. Item 2 - no of calories
+               1. Item 1 - no of calories and total protiens
+               2. Item 2 - no of calories and and total protiens
                ----
                ----
 
@@ -55,7 +54,7 @@ You are an expert in nutritionist where you need to see the food items from the 
 
 if submit:
     image_data=input_image_setup(uploaded_file)
-    response=get_gemini_response(input_prompt,image_data,input)
+    response=get_gemini_response(input_prompt,image_data)
     st.subheader("The Response is")
     st.write(response)
 
